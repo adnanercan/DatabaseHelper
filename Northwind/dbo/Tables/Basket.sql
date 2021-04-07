e1d67ddea072b12b59e74449c339a88dd3f00ad7
Adnan Ercan Ozturk <adnan.ercan@outlook.com>
2021-04-07 11:27:49 +0300

    Create Basket Table

---
+++ Northwind/dbo/Tables/Basket.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Basket]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ProductId] INT NULL, 
+    [CustomerId] NCHAR(5) NULL, 
+    [Quantity] INT NULL, 
+    [Price] MONEY NULL, 
+    [CreateDate] DATETIME NULL Default Getdate()
+)
