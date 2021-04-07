68a2e20c7668b69eee1293533a9914e57089dfe2
Adnan Ercan Ozturk <adnan.ercan@outlook.com>
2021-04-07 11:50:37 +0300

    Add insert StoredProcedure To Basket Table

---
+++ Northwind/dbo/Stored Procedures/AddBasket.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[AddBasket]
+	@ProductId int,
+	@CustomerId varchar(5),
+	@Quantity int,
+	@Price money
+
+AS
+Insert into Basket Values (@ProductId,@CustomerId,@Quantity,@Price)
+
+
